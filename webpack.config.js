7b12dc50aba962a66d0b3f13b03aee54d4500d9d
xxj95719 <87416959@qq.com>
2018-08-07 10:21:17 +0800

    `cross-env`统一Windows与Linux下设置环境变量的方式

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 'use strict'
 // 根据环境变量引用相关的配置文件
 module.exports = (env, argv) => {
-  if (argv.mode === 'development') {
-    process.env.NODE_ENV = 'dev'
-    return require(`./config/webpack.config.dev.js`)
-  } else {
-    process.env.NODE_ENV = 'prod'
-    return require(`./config/webpack.config.prod.js`)
-  }
+  // mode方式
+  // if (argv.mode === 'development') {
+  //   process.env.NODE_ENV = 'dev'
+  //   return require(`./config/webpack.config.dev.js`)
+  // } else {
+  //   process.env.NODE_ENV = 'prod'
+  //   return require(`./config/webpack.config.prod.js`)
+  // }
+  return require(`./config/webpack.config.${process.env.NODE_ENV}.js`) // env方式
 }
