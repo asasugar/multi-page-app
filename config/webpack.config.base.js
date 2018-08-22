fd7e7eb071b8f5422d76c2017fa508da83f1154b
xxj95719 <87416959@qq.com>
2018-08-22 19:01:47 +0800

    html-withimg-loader解决html模板img标签无法被webpack打包

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -64,6 +64,11 @@ module.exports = {
           }
         ]
       },
+      // 解决html模板img标签无法被webpack打包
+      {
+        test: /\.(htm|html)$/i,
+        use: ['html-withimg-loader']
+      },
       {
         test: /\.css$/,
         exclude: /node_modules/,
